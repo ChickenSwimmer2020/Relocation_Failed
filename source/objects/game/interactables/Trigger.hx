bc9d8b5e3c5cb7f889a1063f7ed8e12ecc0e8637
ChickenSwimmer2020 <105177647+ChickenSwimmer2020@users.noreply.github.com>
2025-02-07 08:49:49 -0500

    trigger fix
    triggers should no longer crash the level editor
    updated level editor exit button state switch to flixel 6

--- source/objects/game/interactables/Trigger.hx
+++ source/objects/game/interactables/Trigger.hx
@@ -4,8 +4,9 @@ class Trigger extends FlxSprite {
     public var Function:String;
     public var Width:Float;
     public var Height:Float;
+    public var inEditor:Bool;
 
-    public function new(X:Float, Y:Float, Width:Float, Height:Float, ?IsVisible:Bool = false, Func:String) {
+    public function new(X:Float, Y:Float, Width:Float, Height:Float, ?IsVisible:Bool = false, Func:String, ?EditorMode:Bool = false) {
         super(X, Y);
         if(IsVisible != null && IsVisible == true) {
             this.loadGraphic(Assets.image('Trigger'));
@@ -13,13 +14,16 @@ class Trigger extends FlxSprite {
         this.width = Width;
         this.height = Height;
         Function = Func;
+        inEditor = EditorMode;
     }
     override public function update(elapsed:Float) {
         super.update(elapsed);
-        if(Playstate.instance.Player != null) {
-            if(Playstate.instance.Player.overlaps(this)) {
-                if(Function != null)
-                    trace(Function);
+        if(!inEditor)   {
+            if(Playstate.instance.Player != null) {
+                if(Playstate.instance.Player.overlaps(this)) {
+                    if(Function != null)
+                        trace(Function);
+                }
             }
         }
     }
